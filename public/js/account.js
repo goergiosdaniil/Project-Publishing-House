a438ac738a4bc6b4c7ec392b3b3558f0e2343ba1
spyridon peppas <sipmarch7@hotmail.com>
2020-06-14 18:53:00 +0300

    lil frontend fixes

--- public/js/account.js
+++ public/js/account.js
@@ -1,6 +1,12 @@
 let badgeFull = document.getElementById("badgeFull");
 let badgeGrey = document.getElementById("badgeGrey");
 let totCom = document.getElementById("totalComments");
+let upBtn = document.getElementById("uploadBtn");
+let doc = document.getElementById("document");
+let radioType = document.getElementsByClassName("radioType");
+
+upBtn.disabled = true;
+doc.addEventListener('change',upBtnDisabled);
 
 //kalitera na  ginei sto nodejs auto
 if (totCom.innerHTML < 10){
@@ -9,12 +15,10 @@ if (totCom.innerHTML < 10){
     badgeGrey.style.display = "none";
 }
 
-
 var currStates = document.getElementsByClassName("currentState");
 for (i = 0 ; i<currStates.length ; i++){
     console.log(currStates[i].innerHTML);
     if(currStates[i].innerHTML == "Σε αναμονή"){
-
         currStates[i].style.color ="#0087ff";
     }
     else if(currStates[i].innerHTML == "Εγκρίθηκε"){
@@ -22,5 +26,17 @@ for (i = 0 ; i<currStates.length ; i++){
     }
     else if(currStates[i].innerHTML == "Απορρίφθηκε"){
         currStates[i].style.color ="Red";
+    }
+}
+
+function upBtnDisabled(){
+    if (doc.value==""){
+        upBtn.disabled = true;
+    }else{
+        for (radio of radioType){
+            if (radio.checked===true){
+                upBtn.disabled = false;
+            }
+        } 
     }
 }
